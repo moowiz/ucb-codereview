8eee038f8ff0533c00feb4f2d0d8447f33776994
Akihiro Matsukawa <akihiro.matsukawa@gmail.com>
2012-08-19 18:25:47 -0700

    OOP API for database to abstract away SQL
    Methods to retrieve and update last upload time

---
+++ cmd/model.py
@@ -0,0 +1,60 @@
+"""
+Data model API for the database.
+"""
+
+import sqlite3
+
+class CodeReviewDatabase(object):
+    """
+    An object API to interact with the sqlite3 database. Initialize it with
+    the db path. Be sure to close connections for best performance.
+    """
+
+    def __init__(self, db_path):
+        """
+        Args:
+            db_path: the path to find sqlite db at. see utils.read_db_path.
+        """
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        """
+        Close all open connections
+        """
+        self.cursor.close()
+        self.conn.close()
+
+    def last_uploaded(self):
+        """
+        Retrieve last upload time from database.
+
+        Returns:
+            Unix time in integer if exists, else None
+        """
+        get_last_upload_sql = "SELECT last FROM upload LIMIT 1"
+        result = self.cursor.execute(get_last_upload_sql).fetchone()
+        if result:
+            # if not empty, then result should be tuple with 1 elem
+            return result[0]
+        else:
+            # this clause not really necessary
+            return None
+
+    def set_last_uploaded(self, time_int):
+        """
+        Update the last upload time.
+
+        Args:
+            time_int: integer repr. unix time
+        """
+        if self.last_uploaded():
+            # if an entry exists, then we need an UPDATE command
+            update_last_upload_sql = "UPDATE upload SET last = ?"
+            self.cursor.execute(update_last_upload_sql, time_int)
+            self.conn.commit()
+        else:
+            # if an entry exists, then we need an UPDATE command
+            insert_last_upload_sql = "INSERT INTO upload VALUES (?)"
+            self.cursor.execute(insert_last_upload_sql, time_int)
+            self.conn.commit()
