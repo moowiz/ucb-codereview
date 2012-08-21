f4e53dcc403648512b3030bb9af42a927050898f
Akihiro Matsukawa <akihiro.matsukawa@gmail.com>
2012-08-21 16:03:47 -0700

    Impl + tests to manipulate "roster" table

--- cmd/model.py
+++ cmd/model.py
@@ -58,3 +58,42 @@ class CodeReviewDatabase(object):
             insert_last_upload_sql = "INSERT INTO upload VALUES (?)"
             self.cursor.execute(insert_last_upload_sql, (time_int,))
             self.conn.commit()
+
+    def get_issue_number(self, students, assign):
+        """
+        Gets the issue number for the particular student & assignment
+
+        Args:
+            students: a tuple of student logins,
+                eg. ("cs61a-ab",) or ("cs61a-ab", "cs61a-bc")
+            assign: assignment name, eg. "proj1"
+        """
+        partners = " ".join(sorted(students))
+        get_issue_sql = "SELECT issue FROM roster " + \
+                "WHERE partners=? AND assignment=? LIMIT 1"
+        res_cur = self.cursor.execute(get_issue_sql, (partners, assign))
+        result = res_cur.fetchone()
+        if result:
+            # if not empty, then result should be tuple with 1 elem
+            return result[0]
+        else:
+            # this clause not really necessary
+            return None
+
+    def set_issue_number(self, students, assign, issue_num):
+        """
+        Records the new issue number for the particular student & assignment
+        in the database. Note that this method does not perform checks to
+        see if an entry already exists.
+
+        Args:
+            students: a tuple of student logins,
+                eg. ("cs61a-ab",) or ("cs61a-ab", "cs61a-bc")
+            assign: assignment name, eg. "proj1"
+            issue_num: the issue number, this comes from upload.py
+        """
+        partners = " ".join(sorted(students))
+        set_issue_sql = "INSERT INTO roster (partners, assignment, issue)" + \
+                "VALUES (?, ?, ?)"
+        self.cursor.execute(set_issue_sql, (partners, assign, issue_num))
+        self.conn.commit()
--- cmd/model_test.py
+++ cmd/model_test.py
@@ -38,13 +38,35 @@ class TestCodeReviewDatabase(unittest.TestCase):
         """
         time_val = 1345427105 # just a realistic time val, ~ 8/19/12 6:45pm
         self.assertIsNone(self.model.last_uploaded(),
-                "Did not return None even though entry doesn't exist")
+                "Did not return None even though upload entry doesn't exist")
         self.model.set_last_uploaded(time_val)
         self.assertEquals(time_val, self.model.last_uploaded(),
                 "Created initial val, but appears incorect")
         self.model.set_last_uploaded(time_val + 10)
         self.assertEquals(time_val + 10, self.model.last_uploaded(),
                 "Updated value, but appears incorrect")
+
+    def test_roster(self):
+        """
+        Test that we can manipulate the issues table as expected.
+        """
+        self.assertIsNone(self.model.get_issue_number(("test",), "test"),
+                "Did not return None even though roster entry doesn't exist")
+
+        test_tuples = ( (("cs61a-ab",), "proj1", 1000),
+                        (("cs61a-ab",), "proj2", 1001),
+                        (("cs61a-bc", "cs61a-cd"), "proj1", 2000),
+                        (("cs61a-bc", "cs61a-cd"), "proj2", 2001) )
+
+        # pylint: disable=W0142
+        for test_tuple in test_tuples:
+            self.model.set_issue_number(*test_tuple)
+
+        for students, assign, issue_num in test_tuples:
+            self.assertEquals(issue_num,
+                    self.model.get_issue_number(students, assign),
+                    "Got wrong issue number")
+
 
 
 if __name__ == "__main__":
