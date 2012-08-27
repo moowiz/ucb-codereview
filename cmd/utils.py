acc5cb992c80c514040cfcf940aea8c46a1777c8
Steve <moowiz2020@gmail.com>
2012-08-26 21:04:13 -0700

    Added debug statement to utils.run

--- cmd/utils.py
+++ cmd/utils.py
@@ -10,6 +10,7 @@ def read_db_path():
 
 def run(cmd, content=""):
     """Run a shell command and pass content as stdin."""
+    print "running command {}".format(command)
     proc = Popen(cmd.split(), stdin=PIPE, stdout=PIPE)
     out, err = proc.communicate(input=content)
     if err is not None:
