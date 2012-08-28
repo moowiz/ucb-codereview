fc65610f4513f8a10353d1715b00dbc25aae78ba
Steve <moowiz2020@gmail.com>
2012-08-28 16:22:30 -0700

    Fixed some typos, and made new_submit.py be dumb for now

--- new_submit.py
+++ new_submit.py
@@ -15,9 +15,10 @@ def run_submit(assign):
     # print "running command {}".format(cmd)
     # print "cwd {}".format(os.getcwd())
     cmd = "submit " + assign
-    proc = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate(input=".")
-    print("out {} err {}".format(out, err))
+    subprocess.call(cmd, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+    # proc = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    # out, err = proc.communicate(input=".")
+    # print("out {} err {}".format(out, err))
         
 
 def my_prompt(initial_message, prompt, defaults_file):
@@ -69,13 +70,13 @@ def get_partners():
     """
     Prompts the user for their gmails, and stores the file in the GMAILS_FILE file
     """
-    partners = my_prompt("Enter you and your partner's logins.", "Login", __LOGINS_FILE)
-    write_defaults(partners, __LOGINS_FILE, string_to_join=" ")
+    partners = my_prompt("Enter you and your partner's logins.", "Login", LOGINS_FILE)
+    write_defaults(partners, LOGINS_FILE, string_to_join=" ")
     return partners
 
 def get_sections():
-    sections = my_prompt("Enter you and your partner's section numbers.", "Section Number", __SECTIONS_FILE)
-    write_defaults(sections, __SECTIONS_FILE)
+    sections = my_prompt("Enter you and your partner's section numbers.", "Section Number", SECTIONS_FILE)
+    write_defaults(sections, SECTIONS_FILE)
     return sections
 
 def main(assign):
@@ -83,7 +84,6 @@ def main(assign):
     sections = get_sections()
     partners = get_partners()
     run_submit()
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Submits the assignment, \
