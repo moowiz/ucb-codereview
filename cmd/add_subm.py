d0dc25df6361e61031cfbc9ca2ff95d5d8c27958
Steve <moowiz2020@gmail.com>
2012-08-28 16:41:03 -0700

    Added something to filter out files we don't want to copy from the template

--- cmd/add_subm.py
+++ cmd/add_subm.py
@@ -139,9 +139,11 @@ def upload(path_to_repo, gmails, logins, assign):
     finally:
         os.chdir(original_path)
 
-def copy_important_files(assign, start_dir, end_dir):
+def copy_important_files(assign, start_dir, end_dir, template=False):
     original_path = os.getcwd()
     files_to_copy = get_important_files(assign)
+    if template:
+        files_to_copy = list(filter(lambda x: x not in new_submit.important_files, files_to_copy))
     # print("copying into dir {} with {}".format(end_dir, os.listdir(end_dir)))
     for filename in files_to_copy:
         shutil.copy(start_dir + filename, end_dir + filename)
@@ -168,7 +170,7 @@ def put_in_repo(login, assign):
         path_to_template += assign + "/"
         if not os.path.exists(path_to_template):
             raise Exception("Assignment path {} doesn't exist".format(path_to_template))
-        copy_important_files(assign, path_to_template, path_to_repo)
+        copy_important_files(assign, path_to_template, path_to_repo, template=True)
         git.init(path=path_to_repo)
         git.add(None, path=path_to_repo)
         git.commit("Initial commit", path=path_to_repo)
