086d2521699d666f899a80c090f902abf17dd12b
Stephen Martinis <moowiz2020@gmail.com>
2012-08-29 00:16:13 -0700

    Added a line to ignore

--- cmd/new_submit.py
+++ cmd/new_submit.py
@@ -29,7 +29,8 @@ def run_submit(assign):
             s += c
         return s
     def ignore_line(line):
-        return "Looking for files to turn in...." in line or "Submitting " in line
+        return "Looking for files to turn in...." in line or "Submitting " in line\
+                or "The files you have submitted are:" in line
     def read_line(stream):
         char = get_char(stream)
         s = char
