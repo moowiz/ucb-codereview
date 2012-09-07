b9abe0177002970b14d45fb3bd4e2cd42a910980
Stephen Martinis <moowiz2020@gmail.com>
2012-09-07 03:32:49 -0700

    Fixed typos hopefully.

--- cmd/config.py
+++ cmd/config.py
@@ -1,13 +1,15 @@
+import os
+import sys
 
 #hard coded configs for now. Can move to a config file if we want to.
 _MASTER_DIR = os.path.expanduser('~cs61a/')
-_GRADING_DIR = HOME_DIR + "grading/"
-_SUBMISSION_DIR = HOME_DIR + "submissions/"
-_CODE_REVIEW_DIR = GRADING_DIR + "codereview/"
-_REPO_DIR = CODE_REVIEW_DIR + "repo/"
-_ASSIGN_DIR = HOME_DIR + "lib/"
-_TEMP_DIR = HOME_DIR + "tmp/robot-temp/tmp/"
-_PARAMS_FILE = GRADING_DIR + "params/"
+_GRADING_DIR = _MASTER_DIR + "grading/"
+_SUBMISSION_DIR = _MASTER_DIR + "submissions/"
+_CODE_REVIEW_DIR = _GRADING_DIR + "codereview/"
+_REPO_DIR = _CODE_REVIEW_DIR + "repo/"
+_ASSIGN_DIR = _MASTER_DIR + "lib/"
+_TEMP_DIR = _MASTER_DIR + "tmp/robot-temp/tmp/"
+_PARAMS_FILE = _GRADING_DIR + "params/"
 
 class ConfigException(Exception):
     pass
