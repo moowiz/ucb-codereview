76f84190f404fba473cff8d624319d0495f2b774
Stephen Martinis <moowiz2020@gmail.com>
2012-09-10 02:07:10 -0700

    Forgot to update submit.sh....

--- submit/submit.sh
+++ submit/submit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SUBMIT_PATH="/home/ff/cs61a/grading/codereview/61a-codereview/cmd/submit.py"
+SUBMIT_PATH="/home/ff/cs61a/grading/codereview/61a-codereview/submit/submit.py"
 python3 $SUBMIT_PATH "$@"
