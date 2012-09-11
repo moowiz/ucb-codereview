5c83967ae38d6875274270c59f0492d3b337e29e
Stephen Martinis <moowiz2020@gmail.com>
2012-09-11 00:36:48 -0600

    Fixed how I was calling git status.

--- upload/add_subm.py
+++ upload/add_subm.py
@@ -214,7 +214,10 @@ def put_in_repo(login, assign):
         git.add(None, path=path_to_repo)
         git.commit("Initial commit", path=path_to_repo)
     else: #we want to check that we didnt mess up, and there is actually something here
-        out, err = utils.run("git status", path=path_to_repo)
+        original_path = os.getcwd()
+        os.chdir(path_to_repo)
+        out, err = utils.run("git status")
+        os.chdir(original_path)
         if "fatal: Not a git repository" in err:
             print("Issue number present, but no files in repository. Resetting issue number...")
             model.remove_issue_number(logins, assign, issue_num)
