e7e218e1613cdf720218c945bb28529bec295eda
Stephen Martinis <moowiz2020@gmail.com>
2012-09-13 17:37:44 -0700

    Forgot to save path.

--- upload/add_subm.py
+++ upload/add_subm.py
@@ -210,6 +210,7 @@ def put_in_repo(login, assign):
         git.add(None, path=path_to_repo)
         git.commit("Initial commit", path=path_to_repo)
     else: #we want to check that we didnt mess up, and there is actually something here
+        original_path = os.getcwd()
         os.chdir(path_to_repo)
         out, err = utils.run("git status")
         if "fatal: Not a git repository" in err:
