a9c08d2ac08cd78d52033078a28b76879f9e7b1a
Stephen Martinis <moowiz2020@gmail.com>
2012-10-01 06:40:48 -0700

    Updated grade.py file

--- appengine/grade.py
+++ appengine/grade.py
@@ -1,8 +1,11 @@
 import re
 import logging
 
+from codereview.models import Issue
+
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api import mail
 
 #a regex to filter out lines that are always at the top of the message
 REGEX = re.compile(r'http://.*\.appspot\.com/[0-9]{5}/diff/[0-9]+/[a-zA-Z_]+\.py(#.*)*')
@@ -21,19 +24,22 @@ def is_grade(text_arr):
             return GRADES[grade]
 
 """
+http://berkeley-61a.appspot.com/21161/diff/3001/hog.py File hog.py (right): http://berkeley-61a.appspot.com/21161/diff/3001/hog.py#newcode1 hog.py:1: \"\"\"The Game of Hog\"\"\" Very nice! 
+http://berkeley-61a.appspot.com/18087/diff/1/hw2.py File hw2.py (right): http://berkeley-61a.appspot.com/18087/diff/1/hw2.py#newcode7 hw2.py:7: Excellent!
 http://berkeley-61a.appspot.com/46070/diff/1003/hog.py File hog.py (right): http://berkeley-61a.appspot.com/46070/diff/1003/hog.py#newcode1 hog.py:1: 61A Project 1 Excellent!
 """
+
+__TEXT = []
 
 def grade():
     logging.info("Grading")
     issues = db.GqlQuery("SELECT * FROM Issue WHERE subject='proj1'")
-    logging.info("issues {}".format(str(issues)[:100]))
     for issue in issues:
-        logging.info("issue {}".format(issue))
         message = db.GqlQuery("SELECT * FROM Message WHERE issue = :1 AND draft = false", issue)
         for m in message:
-            logging.info("message {}".format(m.text))
-            mtext = m.text.replace("\n", " ").replace("\r", " ").split(" ")
+            mtext = m.text.split()
+            if len(mtext) < 1:
+                continue
             grade = is_grade(mtext[:2])
             if grade:
                 found_grade(issue, grade)
@@ -43,16 +49,40 @@ def grade():
                     continue
                 else:
                     #fill this in
-                    pass
+                    ind, count = 3, 0
+                    while ind < len(mtext):
+                        if ".py" in mtext[ind]:
+                            if count > 0:
+                                ind += 1
+                                break
+                            else:
+                                count += 1
+                        ind += 1
+                    if "\"\"\"" in mtext[ind]:
+                        ind += 1
+                        while ind < len(mtext):
+                            if "\"\"\"" in mtext[ind]:
+                                count += 1
+                                break
+                            ind += 1
+                    grade = is_grade(mtext[ind:ind+2])
+                    if grade:
+                        found_grade(issue, grade)
         else:
-            print("ERROR: didn't find a grade for issue {}".format(issue))
+            found_grade(issue, None)
 
+def my_mail():
+    address = "cs61a-ta@imail.eecs.berkeley.edu"
+    subject = "grades"
+    body = "".join(__TEXT)
+    mail.send_mail("moowiz2020@gmail.com", address, subject, body)
+    __TEXT.clear()
         
 def found_grade(issue, grade):
     out = ""
     for person in issue.reviewers:
-        out += "{} : {}\n".format(person, grade)
-    print(out)
+        __TEXT.append("{} : {}\n".format(person, grade))
 
 def main(env, response):
     grade()
+    my_mail()
