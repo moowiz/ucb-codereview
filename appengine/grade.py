2bafa48af5c64c0a9d587298091e54cf17dffae1
Stephen Martinis <moowiz2020@gmail.com>
2012-10-25 02:03:56 -0700

    Semi-working grade_style.py

--- appengine/grade.py
+++ appengine/grade.py
@@ -15,16 +15,9 @@ GRADES = {u'very nice' : 2,
             u'ok' : 0,
             u'good job' : 1}
 
-def is_grade(text_arr):
-    text_arr = list(map(lambda x: x.lower().replace("!","").replace(".", "").replace(",", ""), text_arr))
-    text = [" ".join(list(map(lambda x: x.lower(), text_arr))).replace("!", "").replace(".", "")]
-    text.extend(text_arr[:])
-    for grade in GRADES:
-        if grade in text:
-            return GRADES[grade]
 
 """
-http://berkeley-61a.appspot.com/21161/diff/3001/hog.py File hog.py (right): http://berkeley-61a.appspot.com/21161/diff/3001/hog.py#newcode1 hog.py:1: \"\"\"The Game of Hog\"\"\" Very nice! 
+http://berkeley-61a.appspot.com/21161/diff/3001/hog.py File hog.py (right): http://berkeley-61a.appspot.com/21161/diff/3001/hog.py#newcode1 hog.py:1: \"\"\"The Game of Hog\"\"\" Very nice!
 http://berkeley-61a.appspot.com/46070/diff/1003/hog.py File hog.py (right): http://berkeley-61a.appspot.com/46070/diff/1003/hog.py#newcode1 hog.py:1: 61A Project 1 Excellent!
 """
 
@@ -35,40 +28,6 @@ def grade():
     issues = db.GqlQuery("SELECT * FROM Issue WHERE subject='proj1'")
     for issue in issues:
         message = db.GqlQuery("SELECT * FROM Message WHERE issue = :1 AND draft = false", issue)
-        for m in message:
-            mtext = m.text.split()
-            if len(mtext) < 1:
-                continue
-            grade = is_grade(mtext[:2])
-            if grade:
-                found_grade(issue, grade)
-                break
-            else:
-                if not REGEX.match(mtext[0]): #the first thing needs to be the url thing
-                    continue
-                else:
-                    #fill this in
-                    ind, count = 3, 0
-                    while ind < len(mtext):
-                        if ".py" in mtext[ind]:
-                            if count > 0:
-                                ind += 1
-                                break
-                            else:
-                                count += 1
-                        ind += 1
-                    if "\"\"\"" in mtext[ind]:
-                        ind += 1
-                        while ind < len(mtext):
-                            if "\"\"\"" in mtext[ind]:
-                                count += 1
-                                break
-                            ind += 1
-                    grade = is_grade(mtext[ind:ind+2])
-                    if grade:
-                        found_grade(issue, grade)
-        else:
-            found_grade(issue, None)
 
 def my_mail():
     address = "cs61a-ty@imail.eecs.berkeley.edu"
@@ -76,9 +35,7 @@ def my_mail():
     body = "".join(__TEXT)
     mail.send_mail("moowiz2020@gmail.com", address, subject, body)
     __TEXT[:] = []
-        
-def found_grade(issue, grade):
-    __TEXT.append("{} : {}\n".format(issue.key().id(), grade))
+
 
 def main(env, response):
     grade()
--- upload/grade_style.py
+++ upload/grade_style.py
@@ -1,36 +1,94 @@
-import model
-import config
-import utils
-import os
 import argparse
+import sys
+import os
+import re
+import getpass
+from google.appengine.ext.remote_api import remote_api_stub
 
-db = model.CodeReviewDatabase(utils.read_db_path())
+cwd = os.getcwdu()
+cwd = "/".join(cwd.split("/")[:-1]) + "/"
+cwd += "appengine"
+print cwd
+sys.path.append(cwd)
+os.environ['SERVER_SOFTWARE'] = ''
 
-def main(assign, path):
-    if "~" in path:
-        path = os.path.expanduser(path)
-    grades = dict()
-    with open(path) as f:
-        for line in f:
-            if ":" not in line:
+def auth_func():
+    return (u'moowiz2020@gmail.com', getpass.getpass(u'Password:'))
+
+remote_api_stub.ConfigureRemoteApi(None, '/_ah/remote_api', auth_func, 'berkeley-61a.appspot.com')
+from codereview.models import Message, Issue
+
+REGEX = re.compile(r'http://.*\.appspot\.com/[0-9]{5}/diff/[0-9]+/[a-zA-Z_]+\.py(#.*)*')
+
+GRADES = {u'very nice' : 2,
+          u'very good' : 2,
+          u'excellent' : 3,
+          u'ok' : 0,
+          u'good job' : 1}
+
+__TEXT = []
+
+def is_grade(text_arr):
+    text_arr = list(map(lambda x: x.lower().replace("!","").replace(".", "").replace(",", ""), text_arr))
+    text = [" ".join(list(map(lambda x: x.lower(), text_arr))).replace("!", "").replace(".", "")]
+    text.extend(text_arr[:])
+    for grade in GRADES:
+        if grade in text:
+            return GRADES[grade]
+
+def grade(assign):
+    issues = Issue.all().filter(u'subject =', assign).fetch(None)
+    counter = 0
+    for issue in issues:
+        messages = Message.all().filter("issue = ", issue).fetch(None)
+        if counter % 30 == 0:
+            print "tick"
+        counter += 1
+        for m in messages:
+            mtext = m.text.split()
+            if len(mtext) < 1:
                 continue
-            issue_num, score = list(map(lambda x: x.strip(), line.split(":")))
-            if "N" in score:
-                continue
-            score = int(score)
-            issue_num = int(issue_num)
-            logins = db.find_logins(issue_num)
-            for login in logins:
-                grades[login] = score
-    for k, v in grades.items():
-        print("{}\n{}".format(k, v))
+            grade = is_grade(mtext[:2])
+            if grade:
+                found_grade(issue, grade)
+                break
+            else:
+                if not REGEX.match(mtext[0]): #the first thing needs to be the url thing
+                    continue
+                else:
+                    ind, count = 3, 0
+                    while ind < len(mtext):
+                        if ".py" in mtext[ind]:
+                            if count > 0:
+                                ind += 1
+                                break
+                            else:
+                                count += 1
+                        ind += 1
+                    if "\"\"\"" in mtext[ind]:
+                        ind += 1
+                        while ind < len(mtext):
+                            if "\"\"\"" in mtext[ind]:
+                                count += 1
+                                break
+                            ind += 1
+                    grade = is_grade(mtext[ind:ind+2])
+                    if grade:
+                        found_grade(issue, grade)
+        else:
+            found_grade(issue, None)
 
+def found_grade(issue, grade):
+    __TEXT.append("{} : {}".format(issue.key().id(), grade))
+
+def main(assign):
+    grade(assign)
+    for it in __TEXT:
+        print it
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Grades the style for a given assignment")    
+    parser = argparse.ArgumentParser(description="Grades the style for a given assignment")
     parser.add_argument('assign', type=str,
                         help='the assignment to grade')
-    parser.add_argument('path', type=str,
-                        help='the path to the file containing the grades')
     args = parser.parse_args()
-    main(args.assign, args.path)
+    main(args.assign)
