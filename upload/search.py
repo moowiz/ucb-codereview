45bbc9c858ecb463745c8423b7e1cda54970c603
Stephen Martinis <moowiz2020@gmail.com>
2012-10-25 02:18:15 -0700

    Worked on search.py

--- upload/search.py
+++ upload/search.py
@@ -3,22 +3,28 @@ import sqlite3
 import utils
 import argparse
 
-def main(login, assign):
+def main(login, assign, issue):
     conn = sqlite3.connect(utils.read_db_path())
     cursor = conn.cursor()
 
-    query = "SELECT * FROM roster WHERE partners=?"
-    if assign:
-        query += " AND assignment=?"
-        res = cursor.execute(query, (login, assign))
+    query = "SELECT * FROM roster"
+    if issue:
+        query += " WHERE issue=?"
+        res = cursor.execute(query, (issue,))
     else:
-        res = cursor.execute(query, (login,))
+        query += " WHERE partners=?"
+        if assign:
+            query += " AND assignment=?"
+            res = cursor.execute(query, (login, assign))
+        else:
+            res = cursor.execute(query, (login,))
     for row in res.fetchall():
         print(row)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find the issue numbers for a student")
-    parser.add_argument('login', type=str, help='The student\'s login')
-    parser.add_argument('-a','--assign', type=str, help="The assignment to look at")
+    parser.add_argument('-l', '--login', type=str, help='The student\'s login')
+    parser.add_argument('-a', '--assign', type=str, help="The assignment to look at")
+    parser.add_argument('-i', '--issue', type=int, default=0, help="The issue number.")
     args = parser.parse_args()
-    main(args.login, args.assign)
+    main(args.login, args.assign, args.issue)
