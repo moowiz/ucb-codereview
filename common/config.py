bd66927be2786be8425c8a593351087bb0c915ad
Stephen Martinis <moowiz2020@gmail.com>
2012-10-31 01:26:33 -0700

    semi-hack to make it work easy :)

--- common/config.py
+++ common/config.py
@@ -44,44 +44,46 @@ class Config_Class:
         self.IMPORTANT_FILES = (self.GMAILS_FILE, self.SECTIONS_FILE, self.LOGINS_FILE)
         self.DB_PATH = self.CODE_REVIEW_DIR + "codereview_db.sqlite"
 
+    def get_imp_files(self, assignment):
+        if len(assignment) == 4 and assignment[2] != "1":
+            assignment = assignment[:2] + assignment[-1]
+        if len(assignment) == 6 and assignment[4] != "1":
+            assignment = assignment[:4] + assignment[-1]
+        f = open(config.PARAMS_FILE, 'r')
+        lines = f.read().split("\n")
+        f.close()
+        ind = 0
+        for i, line in enumerate(lines):
+            if line.startswith("assign"):
+                ind = i
+                break
+        lines = lines[ind:]
+        #skip the first one, because lines begins with assign
+        spl = "\n".join(lines).split("assign")[1:]
+        files = []
+        for assign_spl in spl:
+            split = list(filter(lambda x: x and x != "\\\n" and x != "\n", assign_spl.split(" ")))
+            assign = split[0]
+            if not assign == assignment:
+                continue
+            split.pop(0)
+            commands, args = split[::2], split[1::2]
+            commands = list(map(lambda x: x if x == "-req" else None, commands))
+            for i, command in enumerate(commands):
+                if command:
+                    files.append(args[i][1:-1])
+            break
+        files = list(map(lambda x: x.replace("\n\n", '').replace("\n", "").replace("'", ""), files))
+        if not files:
+            raise ConfigException("Couldn't find {} in the params file".format(assignment))
+        return files
+
 config = Config_Class()
 
 class ConfigException(Exception):
     pass
 
-def get_imp_files(assignment):
-    if len(assignment) == 4 and assignment[2] != "1":
-        assignment = assignment[:2] + assignment[-1]
-    if len(assignment) == 6 and assignment[4] != "1":
-        assignment = assignment[:4] + assignment[-1]
-    f = open(config.PARAMS_FILE, 'r')
-    lines = f.read().split("\n")
-    f.close()
-    ind = 0
-    for i, line in enumerate(lines):
-        if line.startswith("assign"):
-            ind = i
-            break
-    lines = lines[ind:]
-    #skip the first one, because lines begins with assign
-    spl = "\n".join(lines).split("assign")[1:]
-    files = []
-    for assign_spl in spl:
-        split = list(filter(lambda x: x and x != "\\\n" and x != "\n", assign_spl.split(" ")))
-        assign = split[0]
-        if not assign == assignment:
-            continue
-        split.pop(0)
-        commands, args = split[::2], split[1::2]
-        commands = list(map(lambda x: x if x == "-req" else None, commands))
-        for i, command in enumerate(commands):
-            if command:
-                files.append(args[i][1:-1])
-        break
-    files = list(map(lambda x: x.replace("\n\n", '').replace("\n", "").replace("'", ""), files))
-    if not files:
-        raise ConfigException("Couldn't find {} in the params file".format(assignment))
-    return files
+
 
 def load_params():
     f = open(config.PARAMS_FILE, 'r')
