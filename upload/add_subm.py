573feaf2f5d8f90b0e25a6693e75cd6a9c35f00f
Stephen Martinis <moowiz2020@gmail.com>
2013-02-26 18:43:24 -0800

    Added owner for a bug. Fixed a bug in the upload script.

--- upload/add_subm.py
+++ upload/add_subm.py
@@ -205,8 +205,7 @@ def put_in_repo(data):
         else:
             path_to_template += "projects/"
         if data.git_assign not in config.ASSIGN_TO_NAME_MAP:
-            pass
-            #path_to_template += data.git_assign + "/"
+            path_to_template += data.git_assign + "/"
         else:
             path_to_template += config.ASSIGN_TO_NAME_MAP[data.git_assign] + "/"
         copy_important_files(data, path_to_template, path_to_repo, template=True)
