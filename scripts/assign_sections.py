f88cd07188ae22eb3bb2a5156747413cfd0a80e1
Stephen Martinis <moowiz2020@gmail.com>
2013-04-22 14:29:03 -0700

    Modified scripts

--- scripts/assign_sections.py
+++ scripts/assign_sections.py
@@ -1,36 +1,24 @@
 import base
-parser = argparse.ArgumentParser(description="Creates the email->section mappings for accounts")
-parser.add_argument('assign', type=str,
-                    help='the assignment to grade')
+import argparse
+
+parser = argparse.ArgumentParser(description="Randomly assigns sections to people that don't have any")
 args = base.init(parser)
 
-from codereview.models import Issue, Account
+from codereview.models import Account
+import random
 
-def find_all(assign):
-    with open('out', 'w') as f:
-        issues = Issue.all().filter(u'subject =', assign).fetch(100)
-        for iss in issues:
-            #print iss.reviewers
-            acc = [Account.get_account_for_email(stu) for stu in iss.reviewers]
-            #print acc
-            acc = [a for a in acc if len(str(a)) > 4]
-            #print [a.email for a in acc]
-            sections = [a.sections for a in acc]
-            if sections:
-                all_sections = set(reduce(lambda a, b: a + b, sections))
-                #print all_sections
-                if len(set(all_sections)) > 1:
-                    val = iss.key().id()
-                    print val, iss.sections
-                    f.write("{} {}\n".format(val, iss.sections))
-            else:
-                print 'uhoh'
-                print iss.sections
-        
-
-def main(assign):
-    find_all(assign)
+all_sections = list(range(11,29))
+def main():
+    accs = Account.all()
+    done = 0
+    for i, acc in enumerate(accs):
+    	if i % 10 == 0:
+    		print 'i {} done {}'.format(i, done)
+    	if not acc.sections:
+    		acc.sections = [random.choice(all_sections)]
+    		acc.put()
+    		done += 1
 
 if __name__ == "__main__":
-    main(args.assign)
+	main()
 
