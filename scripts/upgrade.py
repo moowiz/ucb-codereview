608a53f9dd45a9d6f8747cde9817e5cabfc22723
Stephen Martinis <moowiz2020@gmail.com>
2013-07-13 18:57:13 -0700

    Fixed more stuff

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -4,12 +4,23 @@ parser = argparse.ArgumentParser(description="Creates the email->section mapping
 args = base.init(parser)
 
 from codereview.models import Issue, Account
+from google.appengine.ext import db
 
 def find_all():
-    with open('out', 'w') as f:
-        issues = Issue.all()
-        for iss in issues.fetch(10):
-            print(iss.semester)
+	all_ = []
+	count = 0
+	iterr = iter(db.GqlQuery("SELECT * FROM Issue WHERE subject=:1", "proj1").run(batch_size=400))
+	try:
+		while True:
+			a = next(iterr)
+			print a
+			a.semester = u'sp13'
+			print count
+			count += 1
+			all_.append(a)
+	except StopIteration:
+		pass
+	db.put(all_)
         
 
 def main():
