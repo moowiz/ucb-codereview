a6c0742805657660c3396ee59d22314cb9435da7
Robert Huang <toroberthuang@gmail.com>
2013-07-21 00:36:41 -0700

    fixed indentations

--- scripts/transform_grades.py
+++ scripts/transform_grades.py
@@ -8,24 +8,24 @@ parser.add_argument('out_file', type=str)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-	email_to_login = {}
-	for path, __, files in os.walk(os.path.expanduser('~/grading/register/')):
-		for file in files:
-			with open(os.path.join(path, file)) as open_file:
-				lines = open_file.read().split('\n')
-				while 'Email' not in lines[0]:
-					lines = lines[1:]
-				email_line = lines[0]
-				email = email_line[email_line.find(':')+1:].strip()
-				email_to_login[email] = file
+    email_to_login = {}
+    for path, __, files in os.walk(os.path.expanduser('~/grading/register/')):
+        for file in files:
+            with open(os.path.join(path, file)) as open_file:
+                lines = open_file.read().split('\n')
+                while 'Email' not in lines[0]:
+                    lines = lines[1:]
+                email_line = lines[0]
+                email = email_line[email_line.find(':')+1:].strip()
+                email_to_login[email] = file
 
-	with open(args.file) as open_file:
-		with open(args.out_file, 'w') as out_file:
-			for line in open_file:
-				email, grade = line.split(':')
-				grade = int(grade.strip())
-				email = email.strip()
-				try:
-					out_file.write('{} a {}\n'.format(email_to_login[email], grade))
-				except:
-					print('ERR: couldn\'t write grade for', email, ' grade was', grade)
+    with open(args.file) as open_file:
+        with open(args.out_file, 'w') as out_file:
+            for line in open_file:
+                email, grade = line.split(':')
+                grade = int(grade.strip())
+                email = email.strip()
+                try:
+                    out_file.write('{} a {}\n'.format(email_to_login[email], grade))
+                except:
+                    print('ERR: couldn\'t write grade for', email, ' grade was', grade)
