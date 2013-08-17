fed0e5af75ab992e36082d2e98740a5ca5a143e1
Stephen Martinis <moowiz2020@gmail.com>
2013-08-17 08:17:12 -0700

    Ignored virtual_env, updated tests

--- appengine/tests/run_tests.py
+++ appengine/tests/run_tests.py
@@ -51,8 +51,6 @@ def setup_test_env(sdk_path):
   # needed for now to make appstats happy (see comments on
   # http://codereview.appspot.com/5305060/).
   os.environ['REQUEST_ID_HASH'] = 'testing'
-  from google.appengine.dist import use_library
-  use_library('django', '1.2')
 
 
 def main():
