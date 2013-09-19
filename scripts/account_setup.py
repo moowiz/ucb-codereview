8ddf5e533f77e89e8875e378cfb669de37ae4ffc
Stephen Martinis <moowiz2020@gmail.com>
2013-09-18 17:08:26 -0700

    Made account setup better

--- scripts/account_setup.py
+++ scripts/account_setup.py
@@ -2,25 +2,23 @@ import base
 import argparse 
 import os
 
-parser = argparse.ArgumentParser(description="Creates the email->section mappings for accounts")
-parser.add_argument('mapping', type=str,
-                    help='the path to the csv file containing email to section mappings')
-args = base.init(parser)
 
 from codereview.models import Account
 from google.appengine.api import users
 CURRENT_SEMESTER = "fa13"
 
-def make_acc(email, section):
+def make_acc(email, section, staff):
     acc = Account.get_or_insert('<%s>' % email, user=users.User(email), email=email)
     section = int(section)
+    if staff:
+        acc.is_staff = True
     if section not in acc.sections:
         acc.sections.append(section)
     if CURRENT_SEMESTER not in acc.semesters:
         acc.semesters.append(CURRENT_SEMESTER)
     acc.put()
 
-def main(filename):
+def main(filename, staff=False):
     with open(filename) as f:
         split = f.read().split('\r')
         split = list(map(lambda x: x.strip().split(','), split))
@@ -29,8 +27,13 @@ def main(filename):
     for it in split:
         if count % 10 == 0:
             print 'counter {}'.format(count)
-        make_acc(it[0], it[1])
+        make_acc(it[0], it[1], staff)
         count += 1
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Creates the email->section mappings for accounts")
+    parser.add_argument('mapping', type=str,
+                        help='the path to the csv file containing email to section mappings')
+    args = base.init(parser)
+    
     main(os.path.expanduser(args.mapping))
