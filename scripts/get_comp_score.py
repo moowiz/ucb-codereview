b781b88de3b9222983284ff380f348e687e5c7a7
Stephen Martinis <moowiz2020@gmail.com>
2013-10-15 21:19:29 -0700

    Some bug fixes, and adding a issue fixer thing

--- scripts/get_comp_score.py
+++ scripts/get_comp_score.py
@@ -5,6 +5,10 @@ import argparse
 parser = argparse.ArgumentParser(description="Gets the composition scores for an assignment")
 parser.add_argument('assignment', type=str,
                     help='the assignment to grade')
+parser.add_argument('semester', type=str,
+                    help='the semester from which to get assignments to grade')
+parser.add_argument('-f', '--file', type=str,
+                    help='the file to write the grades to')
 args = base.init(parser)
 
 from codereview.models import Issue
@@ -15,13 +19,18 @@ def main():
     for issue in Issue.all().filter('subject =', args.assignment).filter('semester =', args.semester):
         seen += 1
         if issue.comp_score > -1:
-            for stu in issue.reviewers:
+            for stu in issue.owners:
                 good += 1
                 grades[stu] = issue.comp_score
         if seen % 50 == 0 or good % 50 == 0:
             print "good {} seen {}".format(good, seen)
     for k in grades:
         print "{} : {}".format(k, grades[k])
+    if args.file:
+        with open(args.file, 'w') as f:
+            for k in grades:
+                f.write("{} : {}\n".format(k, grades[k]))
+
 
 if __name__ == "__main__":
     main()
